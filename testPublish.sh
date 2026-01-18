7651f215c398ac9b3547319be7a925c38d124862
Tim Buckland <tim.buckland@gmail.com>
2026-01-18 18:57:08 +0000

    fix: test info

--- testPublish.sh
+++ testPublish.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+# Uses different credentials to publish to TestPyPI for testing purposes.
+# Ensure you have a .pypirc file with a [testpypi] section configured,
+# or set TWINE_USERNAME and TWINE_PASSWORD environment variables accordingly.
+
 python -m twine upload --repository testpypi dist/*
